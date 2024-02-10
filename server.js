01d07a3942963db7408e9342a130d0f30c308f0b
lina <linah.ahmed.92@gmail.com>
2024-02-10 16:50:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,32 @@
 // Description: Basic web server to display data from Dexcom G4.  Requires a database that contains
 // the Dexcom SGV data.
 'use strict';
+const env = require('./env')( );
+const http = require('http');
+
+const interval = 5000; // Set the interval in milliseconds (e.g., 5000 ms = 5 seconds)
+const endpoint = env.DM2NSC_SERVER+'/getdata';
+
+function fetchData() {
+  http.get(endpoint, (res) => {
+    let data = '';
+
+    // A chunk of data has been received.
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received.
+    res.on('end', () => {
+      console.log(`CRON: Response from ${endpoint}: ${data}`);
+    });
+  }).on('error', (err) => {
+    console.error(`CRON: Error calling ${endpoint}: ${err.message}`);
+  });
+}
+
+// Set interval to call fetchData
+setInterval(fetchData, interval);
 
 require('./lib/server/server');
 
